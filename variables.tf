7538ab5caf9315a498eebddce01ce570a1c7940b
ololade <akin0098@algonquinlive.com>
2025-06-06 13:09:57 -0400

    Variables

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "labelPrefix" {
+  description = "akin0098"
+  type        = string
+}
+
+variable "region" {
+  default = "canadacentral"
+}
+
+variable "admin_username" {
+  default = "azureadmin"
+}
